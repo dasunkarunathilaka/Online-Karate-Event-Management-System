08102f785b5a2875c28cf36f65a66f57924cf64f
Dasun Karunathilaka <dinindudasun.14@cse.mrt.ac.lk>
2018-08-10 00:27:19 +0530

    Basic html files added.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Event management system added to installed apps.
+    'EventManagementSystem',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
