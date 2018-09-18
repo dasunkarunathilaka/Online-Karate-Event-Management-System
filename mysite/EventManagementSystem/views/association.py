1d93dc79016a5b62a805625db9b15b00f560559d
Dasun Karunathilaka <dinindudasun.14@cse.mrt.ac.lk>
2018-09-19 00:04:27 +0530

    Signup forms for Association, District and Province created.

--- mysite/EventManagementSystem/views/association.py
+++ mysite/EventManagementSystem/views/association.py
@@ -1,5 +1,17 @@
 from django.views.generic import CreateView
 
+from ..models import User
+from ..forms import AssociationSignupForm
+
 
 class AssociationSignUpView(CreateView):
-    pass
+    model = User
+    form_class = AssociationSignupForm
+    template_name = 'event-management-system/registration/associationSignupForm.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'association'
+        return super(AssociationSignUpView, self).get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
